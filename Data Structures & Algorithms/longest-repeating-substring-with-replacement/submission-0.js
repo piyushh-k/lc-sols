dfcaca269147d316fbc31f54f00c78f77b494a1c
piyush <piyushkochar909@gmail.com>
2026-07-08 16:28:37 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let n = s.length;
+        let res = 0;
+        for(let i = 0; i < n; i++){
+            let count = new Map();
+            let maxF = 0;
+            for(let j = i; j < n; j++){
+                count.set(s[j] , (count.get(s[j]) || 0) + 1);
+                maxF = Math.max(maxF , count.get(s[j]));
+                if((j-i+1) - maxF <= k) res = Math.max(res , j-i+1);
+            }
+        }
+        return res;
+    }
+}
