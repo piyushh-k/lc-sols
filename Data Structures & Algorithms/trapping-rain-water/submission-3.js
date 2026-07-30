baaf50e3eb0f1138afc11015ab25b46b4ba82b4c
piyush <piyushkochar909@gmail.com>
2026-07-30 23:41:48 +0530

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let n = height.length;
+        let maxL = -Infinity;
+        let maxR = -Infinity;
+        let leftMax = new Array(n);
+        let rightMax = new Array(n);
+        let trapWater = 0;
+        for (let i = 0; i < n; i++) {
+            maxL = Math.max(maxL, height[i]);
+            leftMax[i] = maxL;
+        }
+        for (let i = n - 1; i >= 0; i--) {
+            maxR = Math.max(maxR, height[i]);
+            rightMax[i] = maxR;
+        }
+        for (let i = 0; i < n; i++) {
+            let water = Math.min(leftMax[i], rightMax[i]);
+            if (water - height[i] > 0) {
+                trapWater += water - height[i];
+            }
+        }
+        return trapWater;
+    }
+}
